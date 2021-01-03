f04fb48ed5cd2330fc4b094138ee8249da45f52f
Hyllesen <stefanhyltoft@gmail.com>
2021-01-03 19:42:45 +0100

    Simple example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useFormik } from "formik";
 
-function App() {
+export default function App() {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+    },
+  });
+
+  const { values, handleChange, handleSubmit } = formik;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          data-testid="firstName"
+          value={values.firstName}
+          onChange={handleChange}
+          id="firstName"
+          type="text"
+        />
+      </form>
     </div>
   );
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { render, screen, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("Input text in formik form passes but with act warning", () => {
+    render(<App />);
+
+    const firstNameInput = screen.getByTestId("firstName");
+
+    userEvent.type(firstNameInput, "MyFirstName");
+
+    expect(firstNameInput.value).toBe("MyFirstName");
+  });
+
+  test("Input text in formik form doesn't pass, but without act warning", () => {
+    render(<App />);
+
+    const firstNameInput = screen.getByTestId("firstName");
+
+    act(() => {
+      userEvent.type(firstNameInput, "MyFirstName");
+    });
+
+    expect(firstNameInput.value).toBe("MyFirstName");
+  });
 });
